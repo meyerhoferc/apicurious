bd7e87ab1971a592db792175379b72d79a284765
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-21 16:50:04 -0600

    Update root view based on current_user

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
-describe "a user visits the homepage" do
-  it "they see a button to log in with reddit" do
-    visit root_path
+describe "homepage" do
+  describe "a user not logged in visits the homepage" do
+    it "they see a button to log in with reddit" do
+      visit root_path
 
-    within("h1") do
-      expect(page).to have_content("Login with Reddit to view your Reddit feed")
+      within("h1") do
+        expect(page).to have_content("Login with Reddit to view your Reddit feed")
+      end
+
+      within(".button") do
+        expect(page).to have_content("Login with Reddit")
+      end
     end
+  end
 
-    within(".button") do
-      expect(page).to have_content("Login with Reddit")
+  describe "a logged in user visits the homepage" do
+    it "they see a list of their subscribed subreddits" do
+      user = Fabricate(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit root_path
+
+      within("h1") do
+        expect(page).to have_content("#{user.username}'s Subreddits")
+        expect(page).to_not have_content("Login with Reddit to view your Reddit feed")
+      end
     end
   end
 end
