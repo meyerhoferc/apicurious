7161f679e1e1eee2d2df5f3dfd7ccd2910415c54
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-22 12:18:33 -0600

    Add request test for RedditSerivce#subreddits

--- spec/services/reddit_service_spec.rb
+++ spec/services/reddit_service_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe RedditService do
-  attr_reader :service
-  before(:each) do
-    @service = RedditService.new(access_token)
-  end
-
   describe "#subreddits" do
-    xit "finds all subreddits for a user" do
-      subreddits = service.subreddits
-      subreddit = subreddits.first
-      expect(subreddit.class).to eq(Subreddit)
+    it "finds all subreddits for a user" do
+      VCR.use_cassette("/services/subreddits_for_user") do
+        user = Fabricate(:user,
+                         access_token: ENV['UPDATED_DUMMY_TOKEN'],
+                         refresh_token: ENV['REFRESH_TOKEN_DUMMY'])
+        service = RedditService.new(user.access_token)
+        subreddits = service.subreddits
+        subreddit = subreddits.first
+        expect(subreddits.count).to eq(25)
+        expect(subreddit.keys).to eq([:kind, :data])
+      end
     end
   end
 end
