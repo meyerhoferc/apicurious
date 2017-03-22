62cbf7a13ad49e75bd4f9b5fabeb6c83a4222d7a
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-22 12:47:46 -0600

    Add user navbar test

--- spec/features/navbar_spec.rb
+++ spec/features/navbar_spec.rb
@@ -19,4 +19,31 @@ describe "navbar" do
 
     expect(current_path).to eq(root_path)
   end
+
+  it "as a logged in user" do
+    user = Fabricate(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+
+    within(".nav-wrapper") do
+      expect(page).to have_link("Reddit Curious")
+      expect(page).to have_link("Logout")
+      expect(page).to have_link(user.username)
+      expect(page).to_not have_link("Login with Reddit")
+    end
+
+    click_on "/u/#{user.username}"
+
+    within(".nav-wrapper") do
+      expect(page).to have_link("Reddit Curious")
+      expect(page).to have_link("Logout")
+      expect(page).to have_link(user.username)
+      expect(page).to_not have_link("Login with Reddit")
+    end
+
+    click_on "Reddit Curious"
+
+    expect(current_path).to eq(root_path)
+  end
 end
