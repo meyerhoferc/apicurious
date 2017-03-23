da3f28c27c4247e9001be10ed499da2234f3c95d
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-22 19:38:28 -0600

    Add feature test to view posts on subreddit

--- spec/features/user_can_view_a_subreddit_spec.rb
+++ spec/features/user_can_view_a_subreddit_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "user can view a subreddit" do
-  it "and all of its related content" do
+  it "and all of its sidebar content" do
     VCR.use_cassette("/features/view_subreddit_show") do
       tucson_subreddit = Subreddit.new("tucson", '/r/tucson', "tucson things")
       pics_subreddit = Subreddit.new("pics", '/r/pics', "pic things")
@@ -21,6 +21,24 @@ describe "user can view a subreddit" do
         expect(page).to have_content("No identifying info or attempts to find info on someone else.")
         expect(page).to have_content("Not a credible news source OR misleading title")
       end
+    end
+  end
+
+  it "sees the last 15 'hot' posts in a subredit" do
+    tucson_subreddit = Subreddit.new("tucson", '/r/tucson', "tucson things")
+    pics_subreddit = Subreddit.new("pics", '/r/pics', "pic things")
+    subreddits = [tucson_subreddit, pics_subreddit]
+    user = Fabricate(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(User).to receive(:subreddit_subscriptions).and_return(subreddits)
+    visit subreddit_path(tucson_subreddit.title)
+
+    within(".posts") do
+      expect(page).to have_content("Hot Posts")
+      expect(page).to have_content("Author: CompletelyLurker")
+      expect(page).to have_link("Buy/Sell/Trade/Housing: March 2017")
+      expect(page).to have_link("31 comments")
+      expect(page).to have_content("12")
     end
   end
 end
