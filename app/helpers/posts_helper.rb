8fd319cb64f0e60d7a59cc00b416157ddf2f4832
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-23 19:13:04 -0600

    Add nested comments and styling

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+  def create_nested_comments(comment)
+    html = content_tag(:ul, class: "collection") do
+      ul_contents = ""
+
+      ul_contents << content_tag(:li, class: "comment comment_#{comment.depth}") do
+        content_tag(:p, "#{comment.author} [#{comment.score} points]", id: "comment_info") + content_tag(:p, "#{comment.body}", id: "comment_#{comment.id}")
+      end
+      if comment.replies.count > 0
+        comment.replies.each { |reply| ul_contents << create_nested_comments(reply) }
+      end
+      ul_contents.html_safe
+    end.html_safe
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,12 +3,14 @@ class Comment
               :score,
               :body,
               :id,
+              :depth,
               :replies
   def initialize(data)
     @author = data[:author]
     @score = data[:score]
     @body = data[:body]
     @id = data[:id]
+    @depth = data[:depth]
     @replies = []
   end
 end
